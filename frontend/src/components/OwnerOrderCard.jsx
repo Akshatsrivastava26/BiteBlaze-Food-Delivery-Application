dc3c89df6e6a57be770473d0fe952d408a1e10ac
Akshatsrivastava26 <akshat.srivastava1564@gmail.com>
2026-03-17 14:54:21 +0530

    feat: update OwnerOrderCard to accept data prop and display user fullname; fix typo in OrderPlaced component

--- frontend/src/components/OwnerOrderCard.jsx
+++ frontend/src/components/OwnerOrderCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 
-function OwnerOrderCard() {
+function OwnerOrderCard({data}) {
   return (
-    <div>
+    <div className='bg-white rounded-lg shadow p-4 space-y-4'>
+      <h2>{data.user.fullname}</h2>
       
     </div>
   )
--- frontend/src/pages/OrderPlaced.jsx
+++ frontend/src/pages/OrderPlaced.jsx
@@ -9,7 +9,7 @@ function OrderPlaced() {
         <FaCircleCheck className='text-green-500 text-6xl mb-4'/>
         <h1 className='text-3xl font-bold text-gray-800 mb-2'>Order Placed Successfully!</h1>
         <p className='text-gray-600 max-w-md mb-6'>Thank you for your purchase. Your order is being prepared.
-            You can track your oder status in the "My Orders" section. We appreciate your business and hope you enjoy your meal! 🍽️
+            You can track your order status in the "My Orders" section. We appreciate your business and hope you enjoy your meal! 🍽️
         </p>
         <button className='bg-[#ff4d2d] hover:bg-[#e64526] text-white px-6 py-3 rounded-lg text-lg font-medium transition cursor-pointer' onClick={() => navigate("/my-orders")}>
             Back to My orders
